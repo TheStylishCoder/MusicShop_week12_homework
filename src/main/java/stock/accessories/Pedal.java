5c52a98109d4dbe122ac2185b34eab4fba8df287
Anna Cunningham <ac232@hotmail.co.uk>
2021-03-26 20:17:15 +0000

    pedal class & tests

---
+++ src/main/java/stock/accessories/Pedal.java
@@ -0,0 +1,12 @@
+package stock.accessories;
+
+public class Pedal extends Accessory {
+    public Pedal(String description, double costPrice, double sellingPrice) {
+        super(description, costPrice, sellingPrice);
+
+    }
+
+    public double calculateMarkup(double costPrice, double sellingPrice) {
+        return sellingPrice - costPrice;
+    }
+}
---
+++ src/test/java/accessoriesTests/PedalTest.java
@@ -0,0 +1,40 @@
+package accessoriesTests;
+
+import org.junit.Before;
+import org.junit.Test;
+import stock.accessories.Pedal;
+
+import static org.junit.Assert.assertEquals;
+
+public class PedalTest {
+
+    Pedal pedal;
+
+    @Before
+    public void before(){
+        pedal = new Pedal("CryBaby Wah Guitar Pedal", 55.30, 82.30);
+    }
+
+    @Test
+    public void hasDescription(){
+        assertEquals("CryBaby Wah Guitar Pedal", pedal.getDescription());
+    }
+
+    @Test
+    public void hasCostPrice(){
+        assertEquals(55.30, pedal.getCostPrice(), 0.01);
+    }
+
+    @Test
+    public void hasSellingPrice(){
+        assertEquals(82.30, pedal.getSellingPrice(), 0.01);
+    }
+
+    @Test
+    public void canCalculateMarkup(){
+        assertEquals(27, pedal.calculateMarkup(pedal.getCostPrice(), pedal.getSellingPrice()), 0.01);
+    }
+
+}
+
+
