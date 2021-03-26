e70a6d2f8bb1a4083c7ad45dc936bfe889113d0e
Anna Cunningham <ac232@hotmail.co.uk>
2021-03-26 18:52:34 +0000

    FrenchHorn class & tests

---
+++ src/main/java/stock/instruments/FrenchHorn.java
@@ -0,0 +1,24 @@
+package stock.instruments;
+
+public class FrenchHorn extends Instrument {
+
+    private String hornType;
+
+    public FrenchHorn(String description, double costPrice, double sellingPrice, String material, String colour, InstrumentType instrumentType, String hornType) {
+        super(description, costPrice, sellingPrice, material, colour, instrumentType);
+        this.hornType = hornType;
+    }
+
+    public String getHornType(){
+        return this.hornType;
+    }
+
+
+    public String play(String sound) {
+        return "I sound like: " + sound;
+    }
+
+    public double calculateMarkup(double costPrice, double sellingPrice) {
+        return sellingPrice - costPrice;
+    }
+}
---
+++ src/test/java/instrumentTests/FrenchHornTest.java
@@ -0,0 +1,64 @@
+package instrumentTests;
+
+import org.junit.Before;
+import org.junit.Test;
+import stock.instruments.FrenchHorn;
+import stock.instruments.InstrumentType;
+
+import static org.junit.Assert.assertEquals;
+
+public class FrenchHornTest {
+
+    FrenchHorn frenchHorn;
+
+    @Before
+    public void before(){
+        frenchHorn = new FrenchHorn("Odyssey Premiere OFH1750BF", 500, 829, "brass", "yellow", InstrumentType.BRASS, "double");
+
+    }
+
+    @Test
+    public void hasADescription(){
+        assertEquals("Odyssey Premiere OFH1750BF", frenchHorn.getDescription());
+    }
+
+    @Test
+    public void hasACostPrice(){
+        assertEquals(500, frenchHorn.getCostPrice(), 0.01);
+    }
+
+    @Test
+    public void hasASellingPrice(){
+        assertEquals(829, frenchHorn.getSellingPrice(), 0.01);
+    }
+
+    @Test
+    public void hasAMaterial(){
+        assertEquals("brass", frenchHorn.getMaterial());
+    }
+
+    @Test
+    public void hasColour(){
+        assertEquals("yellow", frenchHorn.getColour());
+    }
+
+    @Test
+    public void hasInstrumentType(){
+        assertEquals(InstrumentType.BRASS, frenchHorn.getInstrumentType());
+    }
+
+    @Test
+    public void hasHornType(){
+        assertEquals("double", frenchHorn.getHornType());
+    }
+
+    @Test
+    public void canPlay(){
+        assertEquals("I sound like: honk!!", frenchHorn.play("honk!!"));
+    }
+
+    @Test
+    public void canCalculateMarkup(){
+        assertEquals(329, frenchHorn.calculateMarkup(frenchHorn.getCostPrice(), frenchHorn.getSellingPrice()), 0.01);
+    }
+}
