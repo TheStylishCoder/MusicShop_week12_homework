9a34170cf299dbaf5e0708710b7361b6ca382976
Anna Cunningham <ac232@hotmail.co.uk>
2021-03-26 19:01:56 +0000

    Violin class & tests

---
+++ src/main/java/stock/instruments/Violin.java
@@ -0,0 +1,23 @@
+package stock.instruments;
+
+public class Violin extends Instrument{
+
+    private int noOfStrings;
+
+    public Violin(String description, double costPrice, double sellingPrice, String material, String colour, InstrumentType instrumentType, int noOfStrings) {
+        super(description, costPrice, sellingPrice, material, colour, instrumentType);
+        this.noOfStrings = noOfStrings;
+    }
+
+    public int getNoOfStrings() {
+        return noOfStrings;
+    }
+
+    public String play(String sound) {
+        return "I sound like " + sound;
+    }
+
+    public double calculateMarkup(double costPrice, double sellingPrice) {
+        return sellingPrice - costPrice;
+    }
+}
---
+++ src/test/java/instrumentTests/ViolinTest.java
@@ -0,0 +1,64 @@
+package instrumentTests;
+
+import org.junit.Before;
+import org.junit.Test;
+import stock.instruments.InstrumentType;
+import stock.instruments.Violin;
+
+import static org.junit.Assert.assertEquals;
+
+public class ViolinTest {
+
+    Violin violin;
+
+    @Before
+    public void before(){
+        violin = new Violin("Harlequin", 118, 146, "wood", "purple", InstrumentType.STRINGS, 4);
+
+    }
+
+    @Test
+    public void hasADescription(){
+        assertEquals("Harlequin", violin.getDescription());
+    }
+
+    @Test
+    public void hasACostPrice(){
+        assertEquals(118, violin.getCostPrice(), 0.01);
+    }
+
+    @Test
+    public void hasASellingPrice(){
+        assertEquals(146, violin.getSellingPrice(), 0.01);
+    }
+
+    @Test
+    public void hasAMaterial(){
+        assertEquals("wood", violin.getMaterial());
+    }
+
+    @Test
+    public void hasColour(){
+        assertEquals("purple", violin.getColour());
+    }
+
+    @Test
+    public void hasInstrumentType(){
+        assertEquals(InstrumentType.STRINGS, violin.getInstrumentType());
+    }
+
+    @Test
+    public void hasNoOfStrings(){
+        assertEquals(4, violin.getNoOfStrings());
+    }
+
+    @Test
+    public void canPlay(){
+        assertEquals("I sound like pling", violin.play("pling"));
+    }
+
+    @Test
+    public void canCalculateMarkup(){
+        assertEquals(28, violin.calculateMarkup(violin.getCostPrice(), violin.getSellingPrice()), 0.01);
+    }
+}
