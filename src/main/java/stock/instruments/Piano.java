a65357f0d7e44d171c02ff4cc26ef417a36a6628
Anna Cunningham <ac232@hotmail.co.uk>
2021-03-26 19:20:29 +0000

    Piano class & tests

---
+++ src/main/java/stock/instruments/Piano.java
@@ -0,0 +1,23 @@
+package stock.instruments;
+
+public class Piano extends Instrument {
+
+    private String pianoType;
+
+    public Piano(String description, double costPrice, double sellingPrice, String material, String colour, InstrumentType instrumentType, String pianoType) {
+        super(description, costPrice, sellingPrice, material, colour, instrumentType);
+        this.pianoType = pianoType;
+    }
+
+    public String getPianoType() {
+        return pianoType;
+    }
+
+    public String play(String sound) {
+        return "I sound like: " + sound;
+    }
+
+    public double calculateMarkup(double costPrice, double sellingPrice) {
+        return sellingPrice - costPrice;
+    }
+}
---
+++ src/test/java/instrumentTests/PianoTest.java
@@ -0,0 +1,65 @@
+package instrumentTests;
+
+import org.junit.Before;
+import org.junit.Test;
+import stock.instruments.AcousticGuitar;
+import stock.instruments.InstrumentType;
+import stock.instruments.Piano;
+
+import static org.junit.Assert.assertEquals;
+
+public class PianoTest {
+
+    Piano piano;
+
+    @Before
+    public void before(){
+        piano = new Piano("Yamaha CLP765GP", 4000, 4872, "polished ebony", "black", InstrumentType.KEYBOARD, "grand");
+
+    }
+
+    @Test
+    public void hasADescription(){
+        assertEquals("Yamaha CLP765GP", piano.getDescription());
+    }
+
+    @Test
+    public void hasACostPrice(){
+        assertEquals(4000, piano.getCostPrice(), 0.01);
+    }
+
+    @Test
+    public void hasASellingPrice(){
+        assertEquals(4872, piano.getSellingPrice(), 0.01);
+    }
+
+    @Test
+    public void hasAMaterial(){
+        assertEquals("polished ebony", piano.getMaterial());
+    }
+
+    @Test
+    public void hasColour(){
+        assertEquals("black", piano.getColour());
+    }
+
+    @Test
+    public void hasInstrumentType(){
+        assertEquals(InstrumentType.KEYBOARD, piano.getInstrumentType());
+    }
+
+    @Test
+    public void hasPianoType(){
+        assertEquals("grand", piano.getPianoType());
+    }
+
+    @Test
+    public void canPlay(){
+        assertEquals("I sound like: clink", piano.play("clink"));
+    }
+
+    @Test
+    public void canCalculateMarkup(){
+        assertEquals(872, piano.calculateMarkup(piano.getCostPrice(), piano.getSellingPrice()), 0.01);
+    }
+}
